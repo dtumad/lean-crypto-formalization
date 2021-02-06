42bff030e0b6f7460283ebb4122f0fa7e5a17dbe
Devon Tuma <tumax040@umn.edu>
2021-02-06 00:12:38 -0600

    Basic definition of computation with oracle access

--- src/Comp.lean
+++ src/Comp.lean
@@ -1,35 +1,41 @@
 import data.bitvec.basic
-import tactic.induction
 
+/-- Computational monad to extend the base language of Lean for cryptography purposes.
+  `Rnd n` represents a computation of purely random bits, 
+  and `Repeat` can repeat a random computation until some predicate holds -/
 inductive Comp : Type → Sort*
 | Ret {A : Type} [hA : decidable_eq A] : Π (a : A), Comp A
 | Bind {A B : Type} : Π (cb : Comp B) (ca : B → Comp A), Comp A
 | Rnd : ∀ (n : ℕ), Comp (bitvec n)
 | Repeat {A : Type} : Π (p : A → Prop) [decidable_pred p] (ca : Comp A) , Comp A
 
-
 namespace Comp
 open Comp
 
 variables {A B : Type}
 
+/-- Every computation non-constructively gives an element of the return type-/
 def comp_base_exists (ca : Comp A) : A :=
 @Comp.rec_on (λ A _, A) A ca
   (λ _ _ a, a) (λ _ _ _ _ b fa, fa b)
   (λ n, vector.repeat tt n) (λ _ _ _ _ a, a)
 
+/-- Because only `Ret` and `Rnd` terminate computation, and `Ret` requires `decidable_eq A`,
+  every computation must return a type with decidable equality.
+  This needs to be definitional to make `support` fully computable -/
 def decidable_eq_of_Comp (ca : Comp A) : decidable_eq A :=
 @Comp.rec_on (λ A _, decidable_eq A) A ca
   (λ _ h _, h) (λ A B cb ca hcb hca, hca cb.comp_base_exists)
   (λ _, (by apply_instance)) (λ _ _ _ _ h, h)
 
+/-- alias because this situation is very common due to use of `bUnion` in support -/
 def decidable_eq_of_Comp' (cb : Comp B) (ca : B → Comp A) : decidable_eq A :=
-Comp.decidable_eq_of_Comp $ ca cb.comp_base_exists
+Comp.decidable_eq_of_Comp $ Bind cb ca
 
 
 section support
 
--- The support of `Comp A` is a list of elements of `A` with non-zero probability of being computed
+/-- The support of `Comp A` is a list of elements of `A` with non-zero probability of being computed -/
 def support (ca : Comp A) : finset A :=
 ca.rec_on (λ _ _ a, {a}) 
   (λ A B cb ca hcb hca, @finset.bUnion B A (decidable_eq_of_Comp' cb ca) hcb hca)
@@ -64,6 +70,10 @@ end support
 
 section well_formed_Comp 
 
+/-- A computation is well formed if both of the following conditions hold:
+  1 - All sub-computations are well-formed (Trivial for `Ret` and `Rnd`)
+  2 - The computation has non-empty support (Trivial for all but `Repeat`)
+  Such a computation is gaurunteed to have a non-empty support -/
 inductive well_formed_Comp : ∀ {A : Type}, Comp A → Prop
 | well_formed_Ret {A : Type} [hA : decidable_eq A] (a : A) :
     well_formed_Comp (@Ret A hA a)
@@ -73,23 +83,39 @@ inductive well_formed_Comp : ∀ {A : Type}, Comp A → Prop
     well_formed_Comp (Bind cb ca)
 | well_formed_Rnd {n : ℕ} :
     well_formed_Comp (Rnd n)
-| well_formed_Repeat {A : Type} (ca : Comp A) (p : A → Prop) [decidable_pred p] (a : A)
-    (hca : well_formed_Comp ca) (ha : a ∈ (Repeat p ca).support) :
+| well_formed_Repeat {A : Type} (p : A → Prop) [decidable_pred p] (ca : Comp A) 
+    (hca : well_formed_Comp ca) (hpca : (Repeat p ca).support.nonempty) :
     well_formed_Comp (Repeat p ca)
+
+open well_formed_Comp
 
 theorem support_nonempty_of_well_formed_Comp (ca : Comp A)
   (hca : well_formed_Comp ca) : ca.support.nonempty :=
 begin
-  induction hca with _ _ _ _ _ cb ca _ _ hcb_ih hca_ih n _ _ _ _ a _ ha _,
+  induction hca with _ _ _ _ _ cb ca _ _ hcb_ih hca_ih n _ _ _ _ _ ha _,
   { simp },
   { obtain ⟨b, hb⟩ := hcb_ih,
     obtain ⟨a, ha⟩ := hca_ih b hb,
     exact ⟨a, (mem_support_Bind_iff cb ca a).2 ⟨b, hb, ha⟩⟩ },
   { exact ⟨(Rnd n).comp_base_exists, mem_support_Rnd _⟩ },
-  { exact ⟨a, ha⟩ },
+  { exact ha },
 end
 
 end well_formed_Comp
+
+
+section oracle_Comp
+
+inductive oracle_Comp : Type → Type → Type → Sort*
+| oc_query {A B : Type} : Π (a : A), oracle_Comp A B B
+| oc_ret {A B C : Type} : Π (c : C), oracle_Comp A B C
+| oc_bind {A B C D : Type} : Π (oc : oracle_Comp A B C) (od : C → oracle_Comp A B D),
+    oracle_Comp A B D
+| oc_run {A B C A' B' S : Type} [decidable_eq A] [decidable_eq B] [decidable_eq S] :
+    Π (oc : oracle_Comp A B C) (ob : S → A → oracle_Comp A' B' (B × S)) (s : S), 
+      oracle_Comp A' B' (C × S)
+
+end oracle_Comp
 
 end Comp
 
---
+++ src/dist_sem.lean
@@ -0,0 +1,82 @@
+import Comp
+import measure_theory.probability_mass_function
+import data.real.nnreal
+
+import algebra.big_operators.basic
+
+open_locale big_operators nnreal
+
+variables {A B : Type}
+
+namespace Comp
+
+lemma sum_bitvec_eq_one {n : ℕ} : has_sum (λ (v : bitvec n), ((2 : nnreal) ^ n)⁻¹) 1 :=
+begin
+  convert has_sum_fintype (λ (v : bitvec n), ((2 : nnreal) ^ n)⁻¹),
+  rw [finset.sum_const, nsmul_eq_mul, ← div_eq_mul_inv],
+  have : (finset.univ : finset (bitvec n)).card = 2 ^ n :=
+    (by rw [card_vector n, fintype.card_bool] : fintype.card (bitvec n) = 2 ^ n),
+  simp [this, div_self (λ h, two_ne_zero (pow_eq_zero h) : (2 : nnreal) ^ n ≠ 0)],
+end
+
+lemma tsum_ne_zero_iff {α : Type*} (f : α → ℝ≥0) (hf : summable f) :
+  tsum f ≠ 0 ↔ ∃ (a : α), f a ≠ 0 :=
+by rwa [ne.def, tsum_eq_zero_iff hf, not_forall]
+
+section eval_distribution
+
+private noncomputable def eval_distribution' (ca : Comp A) (h : ca.support.nonempty) : 
+  Σ (da : pmf A), plift (∀ (a : A), (da a ≠ 0 ↔ a ∈ ca.support)) :=
+begin
+  refine ca.rec_on _ _ _ _,
+  { 
+    refine λ _ _ a, ⟨pmf.pure a, plift.up (by simp)⟩,
+  },
+  { 
+    refine λ A B ca cb db da, ⟨db.1.bind (λ b, (da b).1), plift.up (λ a, _)⟩,
+    rw [mem_support_Bind_iff, pmf.bind_apply, 
+      tsum_ne_zero_iff _ (pmf.bind.summable db.1 (λ b, (da b).1) a)],
+    split; rintro ⟨b, hb⟩;  
+      cases (da b).2 with hda; specialize hda a; 
+      cases db.2 with hdb; refine ⟨b, _⟩,
+    { simp [not_or_distrib] at hb,
+      rwa [← hdb b, ← hda] },
+    { simp [not_or_distrib],
+      rwa [← hdb b, ← hda] at hb }
+  },
+  { 
+    refine λ n, ⟨⟨λ v, ((2 : nnreal) ^ n)⁻¹, sum_bitvec_eq_one⟩, plift.up (λ v, _)⟩,
+    simp only [support_Rnd, finset.mem_univ, iff_true, ne.def],
+    refine λ hn, two_ne_zero (pow_eq_zero (inv_eq_zero.mp hn) : (2 : nnreal) = 0),
+  },
+  {
+    intros A p hp ca da,
+    sorry,
+  }
+end
+
+/-- The denotational semantics of a `Comp A` is the distribution of resulting values.
+  This distribution is given in the form of a `pmf` on the type `A` of the computation.
+  Defined for any computation with nonempty support, but only meaningful if `ca` is well formed -/
+noncomputable def eval_distribution (ca : Comp A) (hca : ca.support.nonempty) : pmf A :=
+(eval_distribution' ca hca).fst
+
+theorem eval_distribution_ne_zero_iff (ca : Comp A) (hca : ca.support.nonempty) (a : A) :
+  (eval_distribution ca hca a) ≠ 0 ↔ a ∈ ca.support :=
+plift.rec (λ h, h a) (eval_distribution' ca hca).snd
+
+@[simp] lemma eval_distribution_Ret [decidable_eq A] (a : A) (h : (Ret a).support.nonempty) :
+  eval_distribution (Ret a) h = pmf.pure a := rfl
+
+@[simp] lemma eval_distribution_Bind (cb : Comp B) (ca : B → Comp A) 
+  (h : (Bind cb ca).support.nonempty) 
+  (hb : cb.support.nonempty) (ha : ∀ b, (ca b).support.nonempty):
+  eval_distribution (Bind cb ca) h = (eval_distribution cb hb).bind 
+    (λ b, (eval_distribution (ca b) (ha b))) := rfl
+
+@[simp] lemma eval_distribution_Rnd {n : ℕ} (h : (Rnd n).support.nonempty) :
+  eval_distribution (Rnd n) h = ⟨λ v, ((2 : nnreal) ^ n)⁻¹, sum_bitvec_eq_one⟩ := rfl
+
+end eval_distribution
+
+end Comp
