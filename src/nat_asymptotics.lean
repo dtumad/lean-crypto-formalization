a07944683d59fb07c5f2e8b0bc2571801905ef86
Devon Tuma <tumax040@umn.edu>
2021-03-03 15:03:06 -0800

    full proof in the case of rational polynomials

--- src/nat_asymptotics.lean
+++ src/nat_asymptotics.lean
@@ -7,6 +7,8 @@ universes u v
 
 open_locale nnreal
 
+-- Most of this file should eventually be ported to mathlib probably
+
 section nat_asymptotics
 open asymptotics
 
@@ -79,45 +81,7 @@ begin
       exact hM' x hx.2 } }
 end
 
-end is_O_at_top
-
--- lemma polynomial_is_O_pow_degree'' {α β : Type u} [normed_ring α]
---   [H : ordered_add_comm_monoid α]
---   [semilattice_sup β] [semiring β] [nonempty β] [has_lift_t β α]
---   -- [ordered_add_comm_monoid α]
---   (p : polynomial α) (h : ∀ (a b : α), a ≤ b → ∥a∥ ≤ ∥b∥)
---   :
---   is_O (λ (x : β), polynomial.eval ↑x p) 
---     (λ (x : β), (↑x : α) ^ p.nat_degree) filter.at_top :=
--- begin
---   rw is_O_iff,
---   refine ⟨∥ (finsupp.sum p (λ (e : ℕ), id)) ∥, _⟩,
---   rw filter.eventually_at_top,
---   refine ⟨1, λ b hb, _⟩,
---   refine trans _ (norm_mul_le _ _),
---   refine h _ _ _,
---   simp only [finsupp.sum_mul, polynomial.eval_eq_sum],
---   simp only [finsupp.sum],
-  
---   refine @finset.sum_le_sum ℕ α p.support _ _ H _,
-
-
---   -- rw is_O_at_top_iff,
---   -- refine ⟨∥ (finsupp.sum p (λ (e : ℕ), id)) ∥, 1, λ b hb, _⟩,
---   -- refine trans _ (norm_mul_le _ _),
---   -- simp [finsupp.sum_mul, polynomial.eval_eq_sum],
---   -- refine h _ _ _,
---   sorry,
--- end
-
--- @[simp] lemma norm_rat_def {q : ℚ} : ∥q∥ = ∥(q : ℝ)∥ := rfl
-
--- lemma norm_sum_le {α : Type u} [normed_group α] : ℕ :=
--- begin
-
--- end
-
-lemma is_O_add {f f' g : ℚ → ℚ} (hf : is_O f g filter.at_top)
+lemma is_O_at_top_add {f f' g : ℚ → ℚ} (hf : is_O f g filter.at_top)
   (hf' : is_O f' g filter.at_top) : (is_O (f + f') g filter.at_top) :=
 begin
   rw is_O_at_top_iff at hf hf' ⊢,
@@ -130,44 +94,27 @@ begin
   refine add_le_add (hf x hx.1) (hf' x hx.2),
 end
 
-/-- Bootstrapping lemma for `is_O_const_add_iff` -/
-private lemma is_O_const_add_iff' {f g : ℚ → ℚ} {c : ℚ} (hc : c ≥ 0) :
-  is_O f g filter.at_top → is_O (λ x, f x + c) g filter.at_top :=
-begin
-  refine λ h, is_O_add h _,
-  rw is_O_at_top_iff,
-  sorry,
-  
-  
-  
-end
+end is_O_at_top
 
-lemma is_O_const_add_iff {f : ℚ → ℚ} {g : ℚ → ℚ} {c : ℚ} : 
-  is_O (λ x, f x + c) g filter.at_top ↔ is_O f g filter.at_top :=
-begin
-  simp [is_O_at_top_iff],
-  refine ⟨λ h, _, λ h, _⟩,
-  {
-    obtain ⟨M, x₀, h⟩ := h,
-    sorry,
-  },
-  sorry,
-end
-
-@[simp] lemma is_O_at_top_mul_left {f : ℚ → ℚ} {g : ℚ → ℚ} :
-  is_O (λ x, f x * x) (λ x, g x * x) filter.at_top ↔ is_O f g filter.at_top :=
-begin
-  sorry,
-end
-
-lemma helper {a b : ℚ} {c : ℝ} (h : ∥a - b∥ ≤ c) : ∥a∥ ≤ ∥b∥ + c :=
+lemma norm_le_norm_add_const_of_dist_le {α : Type u} [normed_group α]
+  {a b : α} {c : ℝ} (h : dist a b ≤ c) : ∥a∥ ≤ ∥b∥ + c :=
 calc ∥a∥ = ∥a - b + b∥ : by rw sub_add_cancel a b
     ... ≤ ∥a - b∥ + ∥b∥ : norm_add_le (a - b) b
+    ... = (dist a b) + ∥b∥ : by rw normed_group.dist_eq
     ... ≤ c + ∥b∥ : add_le_add h le_rfl
     ... ≤ ∥b∥ + c : by rw (add_comm c ∥b∥)
 
-theorem is_O_of_div_tends_to_finite {f g : ℚ → ℚ} (c : ℚ)
-  (h : filter.tendsto (f / g) filter.at_top (nhds c)) :
+example {a b : ℚ} : a / b ≠ 0 → b ≠ 0 :=
+begin
+  contrapose!,
+  refine λ h, _,
+  rw h,
+  simp,
+end
+
+theorem is_O_at_top_of_div_tends_to_finite {f g : ℚ → ℚ} 
+  (hgf : ∀ᶠ x in filter.at_top, g x = 0 → f x = 0)
+  (c : ℚ) (h : filter.tendsto (f / g) filter.at_top (nhds c)) :
   is_O f g filter.at_top :=
 begin
   rw is_O_at_top_iff,
@@ -176,88 +123,86 @@ begin
   have h' := @h (λ (x : ℚ), ∥x∥ ≤ ∥c∥ + 1) begin
     rw filter.eventually_iff_exists_mem,
     refine ⟨metric.ball c 1, metric.ball_mem_nhds c zero_lt_one, λ y hy, _⟩,
-    rw [metric.mem_ball, normed_field.dist_eq] at hy,
-    exact helper (le_of_lt hy),
+    exact norm_le_norm_add_const_of_dist_le (le_of_lt hy),
   end,
-  rw filter.eventually_at_top at h',
-  obtain ⟨x₀, h⟩ := h',
-  refine ⟨x₀, λ x hx, _⟩,
-  specialize h x hx,
-  simp at h,
-  rw div_le_iff sorry at h,
-  exact h,
+  rw filter.eventually_at_top at h' hgf,
+  obtain ⟨x₀, h⟩ := hgf,
+  obtain ⟨x₀', h'⟩ := h',
+  refine ⟨max x₀ x₀', λ x hx, _⟩,
+  rw max_le_iff at hx,
+  specialize h x hx.1,
+  specialize h' x hx.2,
+  simp only [pi.div_apply, normed_field.norm_div] at h',
+  by_cases hfx : f x = 0,
+  { have : ∥f x∥ = 0 := trans (congr_arg _ hfx) norm_zero,
+    refine this.symm ▸ mul_nonneg (by simpa [hfx] using h') (norm_nonneg (g x)) },
+  { replace h := (mt h) hfx,
+    rwa div_le_iff _ at h',
+    refine lt_of_le_of_ne (norm_nonneg (g x)) (λ h', h _),
+    refine norm_eq_zero.mp h'.symm }
 end
 
-/-- lim ≠ ∞ → is_O should be the method of proof -/
-lemma test (p : polynomial ℚ) (q : polynomial ℚ) (h : p.degree ≤ q.degree) :
+lemma polynomial_exists_max_root (p : polynomial ℚ) (hp : p ≠ 0) :
+  ∃ x₀, ∀ x, p.is_root x → x ≤ x₀ :=
+begin
+  let rootsl : list ℚ := multiset.to_list p.roots,
+  by_cases h : list.maximum (multiset.to_list p.roots) = none,
+  { rw list.maximum_eq_none at h,
+    refine ⟨0, λ a _, _⟩,
+    have : a ∈ (multiset.to_list p.roots),
+    by rwa [multiset.mem_to_list, polynomial.mem_roots hp],
+    rw h at this,
+    refine absurd this (list.not_mem_nil a) },
+  { rw [← ne.def, option.ne_none_iff_exists] at h,
+    obtain ⟨x₀, hx₀⟩ := h,
+    refine ⟨x₀, λ x hx, list.le_maximum_of_mem _ hx₀.symm⟩,
+    rwa [multiset.mem_to_list, polynomial.mem_roots hp] }
+end
+
+lemma polynomial.eventually_no_roots (p : polynomial ℚ) (hp : p ≠ 0) :
+  ∀ᶠ x in filter.at_top, ¬ p.is_root x :=
+begin
+  obtain ⟨x₀, hx₀⟩ := polynomial_exists_max_root p hp,
+  rw filter.eventually_at_top,
+  refine ⟨x₀ + 1, λ x hx h, _⟩,
+  specialize hx₀ x h,
+  refine absurd hx₀ _,
+  rw not_le,
+  refine lt_of_lt_of_le _ hx,
+  rw lt_add_iff_pos_right,
+  refine zero_lt_one,  
+end
+
+lemma eventually_imp_help {p q : ℚ → Prop} {l : filter ℚ}
+  (hpq : ∀ x, p x → q x) (h : ∀ᶠ x in l, p x) : 
+  ∀ᶠ x in l, q x :=
+filter.mem_sets_of_superset h hpq
+
+theorem polynomial.is_O_iff_degree_le (p : polynomial ℚ) (q : polynomial ℚ)
+  (h : p.degree ≤ q.degree) :
   is_O (λ x, polynomial.eval x p) (λ x, polynomial.eval x q) filter.at_top :=
 begin
-  rw le_iff_lt_or_eq at h,
-  cases h,
-  { have := polynomial.div_tendsto_zero_of_degree_lt p q h,
-    refine is_O_of_div_tends_to_finite 0 this },
-  { have := polynomial.div_tendsto_leading_coeff_div_of_degree_eq p q h,
-    refine is_O_of_div_tends_to_finite _ this }
-end
-
-lemma rat_polynomial_is_O_pow_degree (p : polynomial ℚ) :
-  is_O (λ x, polynomial.eval x p) (λ x, x ^ p.nat_degree) filter.at_top :=
-begin
-  refine p.rec_on_horner _ _ _,
-  { simp only [polynomial.nat_degree_zero, pow_zero, polynomial.eval_zero],
-    refine is_O_of_le _ (λ _, (by simpa using zero_le_one)) },
-  {
-    intros p c hp hc h,
-    simp only [polynomial.eval_C, polynomial.eval_add, is_O_const_add_iff],
-    refine h.trans _,
-    rw is_O_at_top_iff,
-    refine ⟨1, 1, λ x hx, _⟩,
-    simp only [one_mul, normed_field.norm_pow],
-    refine pow_le_pow _ _,
-    { refine le_abs.mpr (or.inl (by rwa [← rat.cast_one, rat.cast_le])) },
-    {
-      refine polynomial.nat_degree_le_nat_degree (polynomial.degree_le_degree _),
-      simp only [polynomial.coeff_add, polynomial.coeff_nat_degree],
-      rw polynomial.coeff_C,
-      split_ifs with hp' hp',
-      { rw [polynomial.nat_degree_eq_zero_iff_degree_le_zero,
-          polynomial.degree_le_zero_iff, hp] at hp',
-        rwa [hp', polynomial.leading_coeff_C, zero_add] },
-      { rw [add_zero, ne.def, polynomial.leading_coeff_eq_zero_iff_deg_eq_bot,
-          polynomial.degree_eq_bot],
-        refine λ h, hp' (h.symm ▸ polynomial.nat_degree_zero) }
-    }, 
-  },
-  {
-    intros p hp h,
-    simp only [polynomial.eval_X, polynomial.eval_mul],
-    rw polynomial.nat_degree_mul hp polynomial.X_ne_zero,
-    rw polynomial.nat_degree_X,
-    simp [pow_succ'],
-    exact h,
-  },
-end
-
-lemma polynomial_is_O_pow_degree (p : polynomial ℕ) :
-  is_O (λ x, polynomial.eval x p) (λ x, x ^ p.nat_degree) filter.at_top :=
-begin
-  rw is_O_at_top_iff,
-  refine ⟨∥ (finsupp.sum p (λ (e : ℕ), id)) ∥, 1, λ b hb, _⟩,
-  simp only [norm_nat_def, ← nat.cast_pow, ← nat.cast_mul, nat.cast_le,
-    polynomial.eval_eq_sum, finsupp.sum_mul],
-  refine finset.sum_le_sum (λ x hx, mul_le_mul le_rfl (pow_le_pow hb 
-    (polynomial.le_nat_degree_of_mem_supp x hx)) zero_le' zero_le'),
-end
-
-lemma polynomial_is_O_pow_degree' (p : polynomial ℝ≥0) :
-  is_O (λ x, polynomial.eval x p) (λ x, x ^ p.nat_degree) filter.at_top :=
-begin
-  rw is_O_at_top_iff,
-  refine ⟨∥ (finsupp.sum p (λ (e : ℕ), id)) ∥, 1, λ b hb, _⟩,
-  simp only [norm_nnreal_def, ← nnreal.coe_mul, ← nnreal.coe_pow, nnreal.coe_le_coe,
-    polynomial.eval_eq_sum, finsupp.sum_mul],
-  refine finset.sum_le_sum (λ x hx, mul_le_mul le_rfl (pow_le_pow hb 
-    (polynomial.le_nat_degree_of_mem_supp x hx)) bot_le bot_le),
+  by_cases hp : p = 0,
+  { simpa [hp] using is_O_zero (λ x, polynomial.eval x q) filter.at_top },
+  { have hq : q ≠ 0 := begin
+      refine λ hq, hp _,
+      rw ← polynomial.degree_eq_bot at hp ⊢,
+      refine eq_bot_iff.mpr (trans h (eq_bot_iff.mp _)),
+      rwa polynomial.degree_eq_bot,
+    end,
+    rw le_iff_lt_or_eq at h,
+    cases h,
+    { have := polynomial.div_tendsto_zero_of_degree_lt p q h,
+      refine is_O_at_top_of_div_tends_to_finite _ 0 this,
+      refine eventually_imp_help _ (q.eventually_no_roots hq),
+      refine λ x hx hx', absurd hx' hx,
+      },
+    { have := polynomial.div_tendsto_leading_coeff_div_of_degree_eq p q h,
+      refine is_O_at_top_of_div_tends_to_finite _ _ this,
+      refine eventually_imp_help _ (q.eventually_no_roots hq),
+      refine λ x hx hx', absurd hx' hx,
+      },
+  }
 end
 
 variables {α β : Type*}
@@ -266,38 +211,5 @@ variables {α β : Type*}
 def poly_growth [semiring β] [preorder α] [has_norm α] [has_norm β] 
   [has_lift_t α β] (f : α → β) := 
 ∃ (p : polynomial β), is_O f (λ (a : α), polynomial.eval ↑a p) filter.at_top
-
--- theorem poly_growth_iff (f : ℕ → ℕ) :
---   poly_growth f ↔ ∃ (m : ℕ), is_O f (λ n, n ^ m) filter.at_top :=
--- ⟨λ h, let ⟨p, hp⟩ := h in ⟨p.nat_degree, is_O_at_top_trans hp (polynomial_is_O_pow_degree p)⟩,
---   λ h, let ⟨m, hm⟩ := h in ⟨polynomial.X ^ m, by simpa⟩⟩
-
-theorem poly_growth_iff'' (f : ℕ → ℚ) :
-  poly_growth f ↔ ∃ (m : ℕ), is_O f (λ n, (n : ℚ) ^ m) filter.at_top :=
-begin
-  refine ⟨λ h, _, λ h, _⟩,
-  {
-    obtain ⟨p, hp⟩ := h,
-    refine ⟨p.nat_degree, _⟩,
-    refine is_O.trans hp _,
-  },
-  {
-    sorry,
-  }
-end
-
-theorem poly_growth_iff' (f : ℕ → ℝ≥0) :
-  poly_growth f ↔ ∃ (m : ℕ), is_O f (λ n, (n : nnreal) ^ m) filter.at_top :=
-begin
-  refine ⟨λ h, _, λ h, _⟩,
-  {
-    obtain ⟨p, hp⟩ := h,
-    refine ⟨p.nat_degree, is_O_at_top_trans hp _⟩,
-    sorry,
-  },
-  {
-    sorry,
-  }
-end 
 
 end nat_asymptotics
