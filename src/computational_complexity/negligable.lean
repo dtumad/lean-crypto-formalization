1e628cd0c095dd8faea6a1f71ae0a8da19c98798
Devon Tuma <tumax040@umn.edu>
2021-06-03 12:15:00 -0500

    flesh out theory of negligable functions

--- src/computational_complexity/negligable.lean
+++ src/computational_complexity/negligable.lean
@@ -8,6 +8,8 @@ For convenience, the definition is given in terms of `is_O`,
   with various lemmas to translate back to the definition in terms of constants
 -/
 
+-- TODO: remove namespace and space them explicitly (e.g. negligable.add)
+
 open asymptotics
 
 section to_mathlib
@@ -60,13 +62,24 @@ def negligable (f : ℕ → ℝ) :=
 
 namespace negligable
 
-lemma negligable.trans {f g : ℕ → ℝ} (hg : negligable g)
+lemma negligable_of_is_O {f g : ℕ → ℝ} (hg : negligable g)
   (h : is_O f g filter.at_top) : negligable f :=
 λ c, h.trans $ hg c
 
-lemma negligable.add {f g : ℕ → ℝ} (hf : negligable f) (hg : negligable g) :
+lemma add_negligable {f g : ℕ → ℝ} (hf : negligable f) (hg : negligable g) :
   negligable (f + g) :=
 λ c, (hf c).add $ hg c
+
+@[simp] lemma zero_negligable : negligable (λ n, 0) :=
+λ c, is_O_zero _ _
+
+lemma negligable_of_eventually_le {f g : ℕ → ℝ} (hg : negligable g)
+  (h : ∀ᶠ n in filter.at_top, ∥f n∥ ≤ ∥g n∥) : negligable f :=
+hg.negligable_of_is_O begin
+  rw is_O_iff,
+  use 1,
+  simpa using h,
+end
 
 lemma negligable_of_is_O_pow_neg {f : ℕ → ℝ} (C : ℤ)
   (hf : ∀ (c : ℤ) (hc : c < C), is_O f (λ n, (n : ℝ) ^ c) filter.at_top) :
@@ -212,6 +225,117 @@ begin
     by simp only [← int.of_nat_nat_abs_of_nonpos (le_of_lt hc), fpow_coe_nat],
     simp [this],
   }
-end 
+end
+
+@[simp]
+lemma const_mul_negligable_iff (f : ℕ → ℝ) {c : ℝ} (hc : c ≠ 0) :
+  negligable (λ n, c * f n) ↔ negligable f :=
+begin
+  simp only [negligable],
+  refine forall_congr (λ x, ⟨λ h, _, λ h, _⟩),
+  {
+    refine is_O.trans _ h,
+    refine is_O_self_const_mul c hc f filter.at_top,
+  },
+  {
+    refine is_O.trans _ h,
+    refine is_O_const_mul_self c f filter.at_top,
+  }
+end
+
+@[simp]
+lemma x_mul_negligable_iff (f : ℕ → ℝ) :
+  negligable (λ n, (n : ℝ) * f n) ↔ negligable f :=
+begin
+  refine ⟨λ h, _, λ h c, _⟩,
+  { refine h.negligable_of_eventually_le _,
+    rw filter.eventually_at_top,
+    refine ⟨1, λ x hx, _⟩,
+    simp only [normed_field.norm_mul, real.norm_coe_nat],
+    by_cases hfx : f x = 0,
+    { simp [hfx] },
+    { exact (le_mul_iff_one_le_left (norm_pos_iff.2 hfx)).2 (nat.one_le_cast.2 hx) } },
+  { specialize h (c - 1),
+    refine (is_O.mul (is_O_refl (λ (n : ℕ), (n : ℝ)) filter.at_top) h).trans (is_O_of_le _ _),
+    simp only [normed_field.norm_mul, real.norm_coe_nat, normed_field.norm_fpow],
+    intro x,
+    by_cases hx : (x : ℝ) = 0,
+    { by_cases hc : c = 0,
+      { simp [hx, hc, zero_le_one] },
+      { simp [hx, zero_fpow c hc] } },
+    { rw [mul_comm (x : ℝ), fpow_sub_one hx, mul_assoc, inv_mul_cancel hx, mul_one] } }
+end
+
+@[simp]
+lemma pow_mul_negligable_iff (f : ℕ → ℝ) (c : ℕ) :
+  negligable (λ n, (n : ℝ) ^ c * f n) ↔ negligable f :=
+begin
+  induction c with c hc,
+  { simp only [one_mul, pow_zero] },
+  { simp_rw [← hc, pow_succ, mul_assoc, x_mul_negligable_iff] }
+end
+
+lemma poly_help {p : polynomial ℝ} (hp : 1 ≤ p.degree) (c : ℝ) :
+  ∀ᶠ x in filter.at_top, c ≤ ∥p.eval x∥ :=
+begin
+  sorry,
+end
+
+theorem mul_polynomial_negligable_iff (f : ℕ → ℝ) (p : polynomial ℝ) (hp0 : p ≠ 0) :
+  negligable (λ n, (p.eval n) * f n) ↔ negligable f :=
+begin
+  split,
+  {
+    intro h,
+    by_cases hp : 1 ≤ p.degree,
+    { refine negligable_of_eventually_le h _,
+      suffices : ∀ᶠ (n : ℕ) in filter.at_top, 1 ≤ ∥polynomial.eval ↑n p∥,
+      {
+        refine filter.mem_sets_of_superset this _,
+        intros x hx,
+        simp at ⊢ hx,
+        by_cases hfx : f x = 0,
+        { simp [hfx] },
+        { refine (le_mul_iff_one_le_left (norm_pos_iff.2 hfx)).2 hx }
+      },
+      have := poly_help hp 1,
+      unfold filter.eventually at this ⊢,
+      sorry,
+    },
+    {
+      rw [not_le] at hp,
+      replace hp : p.degree ≤ 0 := sorry,
+      have := polynomial.eq_C_of_degree_le_zero hp,
+      have hpc0 : p.coeff 0 ≠ 0 := begin
+        refine λ h, hp0 (this.trans (by simp only [h, ring_hom.map_zero])),
+      end,
+      rw [this] at h,
+      simp at h,
+      rw const_mul_negligable_iff _ hpc0 at h,
+      exact h,
+    }
+  },
+  {
+    intro h,
+    refine polynomial.induction_on' p _ _,
+    {
+      intros p q hp hq,
+      simp_rw [polynomial.eval_add, add_mul],
+      refine add_negligable hp hq,
+    },
+    {
+      intros n x,
+      by_cases hx : x = 0,
+      { simp only [hx, zero_negligable, zero_mul, polynomial.monomial_zero_right, polynomial.eval_zero] },
+      { simpa only [mul_assoc x, const_mul_negligable_iff _ hx, 
+          pow_mul_negligable_iff, polynomial.eval_monomial] },
+    }
+  }
+end
+
+theorem two_pow_negligable : negligable (λ n, 1 / 2 ^ n) :=
+begin
+  sorry,
+end
 
 end negligable
