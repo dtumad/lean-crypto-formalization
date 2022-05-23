d0e89cdd30792cb0568bf46d5da397d7dd62ccc6
Devon Tuma <dtumad@gmail.com>
2022-05-22 21:32:21 -0500

    comments

--- src/computational_monads/constructions/forking_lemma.lean
+++ src/computational_monads/constructions/forking_lemma.lean
@@ -25,6 +25,40 @@ def fork_sim' : simulation_oracle (uniform_selecting ++ (T →ₒ U)) uniform_se
 (seeded_simulation_oracle uniform_selecting) ⟪++⟫
   (random_simulation_oracle (T →ₒ U) ∘ₛ (caching_simulation_oracle (T →ₒ U)))
 
+/-- Remove parts of the cache after the query chosen to fork on
+  TODO: might need to reverse this first? (double check) otherwise build that into a lower API -/
+def fork_cache : (option $ fin q) → query_log (T →ₒ U) → query_log (T →ₒ U)
+| none log := []
+| (some n) log := log.reverse.take ↑n
+
+/-- Run computation twice, using the same random information for both,
+  responding differently to a query specified by `choose_fork`,
+  and returning the results if `choose_fork` makes the same choice each time -/
+def fork : oracle_comp uniform_selecting (option $ (fin q) × A × (query_log (T →ₒ U)) × A × (query_log (T →ₒ U))) :=
+do {
+  -- run the adversary normally, logging the first oracle and caching the second
+  ⟨x, ⟨log, ⟨cache, ()⟩⟩⟩ ← (simulate fork_sim adversary ([], ([], ()))),
+  -- choose the index of the query to fork on
+  i ← return (choose_fork x cache),
+  -- remove things in the cache after the forking query
+  forked_cache ← return (fork_cache i cache),
+  -- run again, using the same random choices for first oracle, and forked cache
+  ⟨x', ⟨_, ⟨cache', ()⟩⟩⟩ ← (simulate fork_sim' adversary (log.reverse, (forked_cache, ()))),
+  -- check the forking index for the second result
+  i' ← return (choose_fork x' cache'),
+  -- return a value if both runs choose the same cache value and where successful (not `none`)
+  return (if i ≠ none ∧ i = i' then i.map (λ n, (n, x, cache, x', cache')) else none)
+}
+
+/-- `n`th position in both caches has the same input and different outputs-/
+def cache_same_in_diff_out (n : ℕ) (cache : query_log (T →ₒ U)) (cache' : query_log (T →ₒ U)) : Prop :=
+match (cache.nth n, cache'.nth n) with
+| ((some ⟨(), t, u⟩), (some ⟨(), t', u'⟩)) := t = t' ∧ u ≠ u'
+| _ := false
+end
+
+section probabilities
+
 -- TODO: random extra things floating around from random_simulation_oracle
 /--
   Run the adversary and then return the result of what should be forked on.
@@ -39,32 +73,6 @@ do {
 /-- Chance that the pre-fork algorithm has a positive result with `choose_fork` -/
 def acc_prob : ℝ≥0∞ := ⟦ (≠) none | acc_experiment adversary choose_fork ⟧
 
-/-- Remove parts of the cache after the query chosen to fork on
-  TODO: might need to reverse this first? otherwise build that into a lower API -/
-def fork_cache : (option $ fin q) → query_log (T →ₒ U) → query_log (T →ₒ U)
-| none log := []
-| (some n) log := log.take ↑n
-
-/-- Run computation twice, using the same random information for both,
-  responding differently to a query specified by `choose_fork`,
-  and returning the results if `choose_fork` makes the same choice each time -/
-def fork : oracle_comp uniform_selecting (option $ (fin q) × A × (query_log (T →ₒ U)) × A × (query_log (T →ₒ U))) :=
-do {
-  ⟨x, ⟨log, ⟨cache, ()⟩⟩⟩ ← (simulate fork_sim adversary ([], ([], ()))),
-  i ← return (choose_fork x cache),
-  forked_cache ← return (fork_cache i cache),
-  ⟨x', ⟨_, ⟨cache', ()⟩⟩⟩ ← (simulate fork_sim' adversary (log.reverse, (forked_cache, ()))),
-  i' ← return (choose_fork x' cache'),
-  return (if i ≠ none ∧ i = i' then i.map (λ n, (n, x, cache, x', cache')) else none)
-}
-
-/-- `n`th position in both caches has the same input and different outputs-/
-def cache_same_in_diff_out (n : ℕ) (cache : query_log (T →ₒ U)) (cache' : query_log (T →ₒ U)) : Prop :=
-match (cache.nth n, cache'.nth n) with
-| ((some ⟨(), t, u⟩), (some ⟨(), t', u'⟩)) := t = t' ∧ u ≠ u'
-| _ := false
-end
-
 /- successfully got two caches, two adversary outputs, and and integer `n`,
   such that `choose_fork` given `n` for both outputs, and the `n`th position in cache has the same inputs and different outputs.
   Being the result of `choose_fork` should give that the result is a valid signature or whatever.
@@ -78,3 +86,5 @@ end
 
 /-- Chance of forking algorithm being successful -/
 def frk_prob : ℝ≥0∞ := ⟦ fork_success choose_fork | fork adversary choose_fork ⟧
+
+end probabilities
