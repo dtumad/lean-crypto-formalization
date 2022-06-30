fe1ffd4319cb5ef0a4198395345311bd9cbad8f8
Devon Tuma <dtumad@gmail.com>
2022-06-29 21:21:37 -0500

    simulation lemmas

--- src/computational_monads/coercions.lean
+++ src/computational_monads/coercions.lean
@@ -1,84 +1,141 @@
 import computational_monads.constructions.uniform_select
 import computational_monads.simulation_semantics.simulation_oracles
 
+/-!
+# Coercions Between Computations With Different Oracle Access
+
+This file provides a number of `has_coe` instances for different `oracle_comp` computations.
+
+The main coercions are for the append operation on `oracle_spec`,
+  allowing an increase the number of oracles in a number of ways.
+This allows a computation to be written by composing computations using fewer oracles.
+-/
+
 open oracle_comp oracle_spec
 
-variables (spec spec' spec'' : oracle_spec) (A : Type)
+variables (A B : Type) (spec spec' spec'' spec''' : oracle_spec)
+  (coe_spec coe_spec' coe_spec'' coe_spec''' : oracle_spec)
+
+section uniform_select
+
+/-- coerce a coin flip into a uniform random selection of a `bool` -/
+noncomputable instance coe_coin_uniform_select :
+  has_coe (oracle_comp coin_oracle A) (oracle_comp uniform_selecting A) :=
+{ coe := λ oa, oa.simulate' ⟪λ _ _, $ᵗ bool⟫ () }
+
+noncomputable example : oracle_comp uniform_selecting bool :=
+do {b ← coin, b' ←$ᵗ bool, return (band b b')}
+
+lemma coe_coin_uniform_select_def (oa : oracle_comp coin_oracle A) :
+  (oa : oracle_comp uniform_selecting A) = oa.simulate' ⟪λ _ _, $ᵗ bool⟫ () := rfl
+
+@[simp]
+lemma eval_distribution_coe_coin_uniform_select :
+  Π {A : Type} (oc : oracle_comp coin_oracle A),
+    ⟦(↑oc : oracle_comp uniform_selecting A)⟧ = ⟦oc⟧
+| _ (pure' A a) := by simp [coe_coin_uniform_select_def, pmf.pure_map]
+| _ (bind' A B oa ob) := sorry
+| _ (query i t) := sorry
+
+end uniform_select
+
+section coe_append
 
 instance coe_append_right :
   has_coe (oracle_comp spec A) (oracle_comp (spec ++ spec') A) :=
 { coe := λ oc, oc.simulate' (stateless_simulation_oracle spec (spec ++ spec')
       (λ i t, let i' : (spec ++ spec').ι := sum.inl i in query i' t)) () }
 
-instance coe_append_right' [has_coe (oracle_comp spec A) (oracle_comp spec' A)] :
-  has_coe (oracle_comp spec A) (oracle_comp (spec' ++ spec'') A) :=
-{ coe := λ oc, ↑(oc : oracle_comp spec' A) }
-
 instance coe_append_left :
   has_coe (oracle_comp spec A) (oracle_comp (spec' ++ spec) A) :=
-{ coe := λ oc, oc.simulate' (stateless_simulation_oracle spec (spec' ++ spec)
-      (λ i t, let i' : (spec' ++ spec).ι := sum.inr i in query i' t)) () }
+{ coe := λ oc, oc.simulate'
+    ⟪ (λ i t, let i' : (spec' ++ spec).ι := sum.inr i in query i' t) ⟫ () }
 
-instance coe_append_left' [has_coe (oracle_comp spec A) (oracle_comp spec' A)] :
-  has_coe (oracle_comp spec A) (oracle_comp (spec'' ++ spec') A) :=
-{ coe := λ oc, ↑(oc : oracle_comp spec' A) }
+instance coe_right_side_append [∀ A, has_coe (oracle_comp coe_spec A) (oracle_comp coe_spec' A)] :
+  has_coe (oracle_comp (spec ++ coe_spec) A) (oracle_comp (spec ++ coe_spec') A) :=
+{ coe := λ oc, oc.simulate' ⟪λ i, sum.rec
+    (λ i' t, (query i' t : oracle_comp (spec ++ coe_spec') (spec.range i')))
+    (λ i' t, (query i' t : oracle_comp (spec ++ coe_spec') (coe_spec.range i')))
+  i⟫ () }
 
-/-- TODO: This `coe` instance might not be really useful? should test for real use cases -/
-instance coe_spec_assoc :
+instance coe_left_side_append [∀ A, has_coe (oracle_comp coe_spec A) (oracle_comp coe_spec' A)] :
+  has_coe (oracle_comp (coe_spec ++ spec) A) (oracle_comp (coe_spec' ++ spec) A) :=
+{ coe := λ oc, oc.simulate' ⟪λ i, sum.rec
+    (λ i' t, (query i' t : oracle_comp (coe_spec' ++ spec) (coe_spec.range i')))
+    (λ i' t, (query i' t : oracle_comp (coe_spec' ++ spec) (spec.range i')))
+  i⟫ () }
+
+instance coe_both_sides_append [∀ A, has_coe (oracle_comp coe_spec A) (oracle_comp coe_spec' A)]
+  [∀ A, has_coe (oracle_comp coe_spec'' A) (oracle_comp coe_spec''' A)] :
+  has_coe (oracle_comp (coe_spec ++ coe_spec'') A) (oracle_comp (coe_spec' ++ coe_spec''') A) :=
+{ coe := λ oc, oc.simulate' ⟪λ i, sum.rec
+    (λ i' t, (query i' t : oracle_comp (coe_spec' ++ coe_spec''') (coe_spec.range i')))
+    (λ i' t, (query i' t : oracle_comp (coe_spec' ++ coe_spec''') (coe_spec''.range i')))
+  i⟫ () }
+
+instance coe_append_right_of_coe [has_coe (oracle_comp coe_spec A) (oracle_comp coe_spec' A)] :
+  has_coe (oracle_comp coe_spec A) (oracle_comp (coe_spec' ++ spec'') A) :=
+{ coe := coe }
+
+instance coe_append_left_of_coe [has_coe (oracle_comp coe_spec A) (oracle_comp coe_spec' A)] :
+  has_coe (oracle_comp coe_spec A) (oracle_comp (spec'' ++ coe_spec') A) :=
+{ coe := coe }
+
+@[priority 1001]
+instance coe_append_assoc :
   has_coe (oracle_comp (spec ++ (spec' ++ spec'')) A) (oracle_comp (spec ++ spec' ++ spec'') A) :=
 ⟨λ oc, oc.simulate' ⟪λ i, match i with 
 | (sum.inl i) :=
-  λ t, let i' : (spec ++ spec' ++ spec'').ι := sum.inl (sum.inl i) in query i' t
+    λ t, let i' : (spec ++ spec' ++ spec'').ι := sum.inl (sum.inl i) in query i' t
 | (sum.inr (sum.inl i)) :=
-  λ t, let i' : (spec ++ spec' ++ spec'').ι := sum.inl (sum.inr i) in query i' t
+    λ t, let i' : (spec ++ spec' ++ spec'').ι := sum.inl (sum.inr i) in query i' t
 | (sum.inr (sum.inr i)) := 
-  λ t, let i' : (spec ++ spec' ++ spec'').ι := sum.inr i in query i' t
+    λ t, let i' : (spec ++ spec' ++ spec'').ι := sum.inr i in query i' t
 end⟫ ()⟩
 
-noncomputable instance coe_coin_uniform_select :
-  has_coe (oracle_comp coin_oracle A) (oracle_comp uniform_selecting A) :=
-{ coe := λ oa, simulate' (stateless_simulation_oracle coin_oracle uniform_selecting (λ _ _, uniform_select_fintype bool)) oa () }
+section examples
+
+variable [h : ∀ A, has_coe (oracle_comp coe_spec A) (oracle_comp coe_spec' A)]
+include h
+
+-- This set of examples serves as sort of a "unit test" for the coercions above
+
+-- coerce a single `coin_oracle` and then append extra oracles
+example (oa : oracle_comp coe_spec A) :
+  oracle_comp (coe_spec' ++ spec' ++ spec'') A := ↑oa
+example (oa : oracle_comp coe_spec A) :
+  oracle_comp (spec ++ coe_spec' ++ spec') A := ↑oa
+example (oa : oracle_comp coe_spec A) :
+  oracle_comp (spec ++ spec' ++ coe_spec') A := ↑oa
+
+-- coerce left side of append and then append on additional oracles
+example (oa : oracle_comp (coe_spec ++ spec) A) :
+  oracle_comp (coe_spec' ++ spec ++ spec') A := ↑oa
+example (oa : oracle_comp (coe_spec ++ spec) A) :
+  oracle_comp (coe_spec' ++ spec' ++ spec) A := ↑oa
+example (oa : oracle_comp (coe_spec ++ spec) A) :
+  oracle_comp (spec' ++ coe_spec' ++ spec) A := ↑oa
+
+-- coerce right side of append and then append on additional oracles
+example (oa : oracle_comp (spec ++ coe_spec) A) :
+  oracle_comp (spec ++ coe_spec' ++ spec') A := ↑oa
+example (oa : oracle_comp (spec ++ coe_spec) A) :
+  oracle_comp (spec ++ spec' ++ coe_spec') A := ↑oa
+example (oa : oracle_comp (spec ++ coe_spec) A) :
+  oracle_comp (spec' ++ spec ++ coe_spec') A := ↑oa
+
+-- coerce an inside part while also applying associativity
+example (oa : oracle_comp (spec ++ (spec' ++ coe_spec)) A) :
+  oracle_comp (spec ++ spec' ++ coe_spec') A := ↑oa
+example (oa : oracle_comp (spec ++ (coe_spec ++ spec')) A) :
+  oracle_comp (spec ++ coe_spec' ++ spec') A := ↑oa
+example (oa : oracle_comp (coe_spec ++ (spec ++ spec')) A) :
+  oracle_comp (coe_spec' ++ spec ++ spec') A := ↑oa
+
+end examples
+
+end coe_append
 
 /-- coercion makes it possible to mix computations on individual oracles -/
 noncomputable example {spec : oracle_spec} : oracle_comp (uniform_selecting ++ spec) bool := 
 do { n ←$[0..10], if n = 0 then return ff else coin }
-
-instance simulation_oracle.coe (spec spec' : oracle_spec) :
-  has_coe (simulation_oracle spec spec') (simulation_oracle spec (spec ++ spec')) :=
-{ coe := λ so, {
-  S := so.S,
-  o := λ i x, ↑(so.o i x)
-}}
-
-instance c' (spec spec' : oracle_spec) :
-  has_coe (simulation_oracle spec spec') (simulation_oracle spec (spec' ++ spec)) :=
-{ coe := λ so, {
-  S := so.S,
-  o := λ i x, ↑(so.o i x)
-}}
-
-instance c'' (spec spec' spec'' : oracle_spec) :
-  has_coe (simulation_oracle spec spec') (simulation_oracle spec (spec' ++ spec'')) :=
-{ coe := λ so, {
-  S := so.S,
-  o := λ i x, ↑(so.o i x)
-}}
-
-instance c''' (spec spec' spec'' : oracle_spec) :
-  has_coe (simulation_oracle spec spec') (simulation_oracle spec (spec'' ++ spec')) :=
-{ coe := λ so, {
-  S := so.S,
-  o := λ i x, ↑(so.o i x)
-}}
-
-noncomputable example {A B C D : Type} [inhabited B] [inhabited C] [fintype C] [decidable_eq C] :
-  simulation_oracle ((A →ₒ C) ++ (unit →ₒ C))
-    ((A →ₒ B) ++ (B →ₒ C) ++ uniform_selecting) :=
-↑(stateless_simulation_oracle (A →ₒ C) ((A →ₒ B) ++ (B →ₒ C))
-  (λ i a, do {
-    b ← query (sum.inl ()) a,
-    c ← query (sum.inr ()) b,
-    return c
-}))
-  ⟪++⟫
-(random_simulation_oracle (unit →ₒ C))
--- src/computational_monads/simulation_semantics/simulate.lean
+++ src/computational_monads/simulation_semantics/simulate.lean
@@ -1,4 +1,5 @@
 import computational_monads.oracle_comp
+import computational_monads.distribution_semantics.eval_distribution
 
 namespace oracle_comp
 
@@ -11,6 +12,8 @@ structure simulation_oracle (spec spec' : oracle_spec) :=
 (S : Type)
 (o (i : spec.ι) : (spec.domain i × S) → oracle_comp spec' (spec.range i × S))
 
+variables (so : simulation_oracle spec spec')
+
 section simulate
 
 /-- Simulate an oracle comp to an oracle comp with a different spec.
@@ -21,26 +24,12 @@ def simulate {spec spec' : oracle_spec} (so : simulation_oracle spec spec') :
 | _ (bind' A B oa ob) state := simulate oa state >>= λ x, simulate (ob x.1) x.2
 | _ (query i t) state := so.o i (t, state)
 
-/-- Get the result of simulation without returning the internal oracle state -/
-@[reducible, inline]
-def simulate' (so : simulation_oracle spec spec') (oa : oracle_comp spec A) (s : so.S) :
-  oracle_comp spec' A :=
-prod.fst <$> oa.simulate so s
-
-def sim_comp {spec spec' spec'' : oracle_spec}
-  (so : simulation_oracle spec spec') (so' : simulation_oracle spec' spec'') :
-  simulation_oracle spec spec'' :=
-{ S := so.S × so'.S,
-  o := λ i ⟨x, s⟩, begin
-    sorry
-  end
-}
-
-variables (so : simulation_oracle spec spec')
-
 @[simp]
 lemma simulate_pure (a : A) (s : so.S) :
   simulate so (pure a) s = pure (a, s) := rfl
+
+lemma eval_distribution_simulate_pure [spec'.finite_range] (a : A) (s : so.S) :
+  ⟦simulate so (pure a) s⟧ = pmf.pure (a, s) := rfl
 
 lemma simulate_pure' (a : A) (s : so.S) :
   simulate so (pure' A a) s = pure' (A × so.S) (a, s) := rfl
@@ -56,9 +45,37 @@ lemma simulate_bind' (oa : oracle_comp spec A) (ob : A → oracle_comp spec B) (
   simulate so (bind' A B oa ob) s = bind (simulate so oa s) (λ x, simulate so (ob x.1) x.2) := rfl
 
 @[simp]
-lemma simulate_query  (i : spec.ι) (t : spec.domain i) (s : so.S) :
+lemma simulate_query (i : spec.ι) (t : spec.domain i) (s : so.S) :
   simulate so (query i t) s = so.o i (t, s) := rfl
 
 end simulate
+
+section simulate'
+
+/-- Get the result of simulation without returning the internal oracle state -/
+@[reducible, inline]
+def simulate' (so : simulation_oracle spec spec') (oa : oracle_comp spec A) (s : so.S) :
+  oracle_comp spec' A :=
+prod.fst <$> oa.simulate so s
+
+@[simp]
+lemma simulate'_pure (a : A) (s : so.S) :
+  simulate' so (pure a) s = prod.fst <$> (pure (a, s)) := rfl
+
+@[simp]
+lemma simulate'_bind (oa : oracle_comp spec A) (ob : A → oracle_comp spec B) (s : so.S) :
+  simulate' so (oa >>= ob) s =
+    prod.fst <$> ((simulate so oa s) >>= (λ x, simulate so (ob x.1) x.2)) := rfl
+
+@[simp]
+lemma simulate'_query (i : spec.ι) (t : spec.domain i) (s : so.S) :
+  simulate' so (query i t) s = prod.fst <$> so.o i (t, s) := rfl
+
+@[simp]
+lemma eval_distribution_simulate'_pure [spec'.finite_range] (a : A) (s : so.S) :
+  ⟦(simulate' so (pure a) s)⟧ = pmf.pure a :=
+by simp only [pmf.pure_map, simulate'_pure, eval_distribution_map, eval_distribution_return]
+
+end simulate'
 
 end oracle_comp
