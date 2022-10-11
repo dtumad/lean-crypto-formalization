b7d1a356d90d8949ea15af61fada76372c6ba0b2
Devon Tuma <dtumad@gmail.com>
2022-10-10 20:56:00 -0500

    mathlib

--- src/to_mathlib/general.lean
+++ src/to_mathlib/general.lean
@@ -8,11 +8,10 @@ import analysis.convex.specific_functions
 # Misc Lemmas That Ideally Should Port to Mathlib
 -/
 
-open_locale measure_theory nnreal ennreal big_operators classical
+open_locale nnreal ennreal big_operators classical
 
-variables {α β γ : Type*} {n : ℕ}
+variables {α β γ : Type*}
 
-@[simp]
 lemma singleton_eq_top_of_subsingleton {α : Type*} [subsingleton α] (x : α) : ({x} : set α) = ⊤ :=
 set.ext (λ y, by simp only [set.mem_singleton_iff,
   eq_iff_true_of_subsingleton, set.top_eq_univ, set.mem_univ])
@@ -21,7 +20,7 @@ lemma finset.count_to_list [decidable_eq α] (s : finset α) (a : α) :
   s.to_list.count a = if a ∈ s then 1 else 0 :=
 by simp only [list.count_eq_of_nodup s.nodup_to_list, finset.mem_to_list]
 
-section tsum
+section tsum_prod
 
 lemma tsum_prod_eq_tsum_snd {α β γ : Type*} [add_comm_monoid α] [topological_space α] [t2_space α]
   {f : β × γ → α} (b : β) (h : ∀ b' ≠ b, ∀ c, f (b', c) = 0) :
@@ -42,30 +41,17 @@ begin
     simp only [prod.mk.inj_iff, eq_self_iff_true, true_and, hc] }
 end
 
+end tsum_prod
+
 namespace ennreal
 
--- TSUMS
-lemma to_nnreal_tsum_eq {α : Type*} (f : α → ℝ≥0∞) :
-  (∑' x, f x).to_nnreal = if ∃ x, f x = ⊤ then 0 else ∑' x, (f x).to_nnreal :=
-begin
-  split_ifs with hf,
-  { exact (to_nnreal_eq_zero_iff _).2 (or.inr (ennreal.tsum_eq_top_of_eq_top hf)) },
-  { rw [nnreal.tsum_eq_to_nnreal_tsum],
-    have : ∀ x, f x ≠ ⊤ := not_exists.1 hf,
-    exact congr_arg ennreal.to_nnreal (tsum_congr $ λ x, (coe_to_nnreal (this x)).symm) }
-end
+section tsum
 
-lemma to_real_tsum_eq {α : Type*} (f : α → ℝ≥0∞) :
-  (∑' x, f x).to_real = if ∃ x, f x = ⊤ then 0 else ∑' x, (f x).to_real :=
-calc (∑' x, f x).to_real = ↑((∑' x, f x).to_nnreal) : rfl
-  ... = ↑(ite (∃ x, f x = ⊤) 0 (∑' x, (f x).to_nnreal)) : congr_arg coe (to_nnreal_tsum_eq f)
-  ... = ite (∃ x, f x = ⊤) 0 ↑(∑' x, (f x).to_nnreal) : by rw [apply_ite coe, nonneg.coe_zero]
-  ... = ite (∃ x, f x = ⊤) 0 (∑' x, (f x).to_real) : congr_arg _ nnreal.coe_tsum
-
--- `to_nnreal_tsum`
 lemma to_nnreal_tsum {α : Type*} {f : α → ℝ≥0∞} (hf : ∀ x, f x ≠ ⊤) :
   (∑' x, f x).to_nnreal = ∑' x, (f x).to_nnreal :=
-(to_nnreal_tsum_eq f).trans (if_neg (not_exists.2 hf))
+calc (∑' x, f x).to_nnreal = (∑' (b : α), (((f b).to_nnreal) : ℝ≥0∞)).to_nnreal :
+    congr_arg ennreal.to_nnreal (tsum_congr $ λ x, (coe_to_nnreal (hf x)).symm)
+  ... = ∑' x, (f x).to_nnreal : by rw [← nnreal.tsum_eq_to_nnreal_tsum]
 
 lemma to_nnreal_tsum_coe {α : Type*} {f : α → ℝ≥0} :
   (∑' x, (f x : ℝ≥0∞)).to_nnreal = ∑' x, f x :=
@@ -73,10 +59,12 @@ trans (to_nnreal_tsum $ λ x, ennreal.coe_ne_top) (tsum_congr $ λ x, ennreal.to
 
 lemma to_real_tsum {α : Type*} {f : α → ℝ≥0∞} (hf : ∀ x, f x ≠ ⊤) :
   (∑' x, f x).to_real = ∑' x, (f x).to_real :=
-by simp_rw [ennreal.to_real, to_nnreal_tsum hf, nnreal.coe_tsum]
+calc (∑' x, f x).to_real = ↑((∑' x, f x).to_nnreal) : rfl
+  ... = ∑' x, (f x).to_real : by simpa only [to_nnreal_tsum hf, nnreal.coe_tsum]
 
+end tsum
 
-
+section sum
 
 -- SUMS
 lemma sum_eq_tsum_indicator {α β : Type*} [add_comm_monoid β] [topological_space β] [t2_space β]
@@ -91,82 +79,52 @@ lemma sum_eq_top_of_eq_top {α : Type*} {f : α → ℝ≥0∞} {s : finset α} 
 (sum_eq_tsum_indicator f s).trans (let ⟨x, hx, hxf⟩ := hf in ennreal.tsum_eq_top_of_eq_top
   ⟨x, trans (set.indicator_apply_eq_self.2 (λ hx', (hx' $ finset.mem_coe.2 hx).elim)) hxf⟩)
 
-
-
--- indicators
-lemma to_nnreal_indicator {α : Type*} (s : set α) (f : α → ℝ≥0∞) (x : α) :
-  (s.indicator f x).to_nnreal = s.indicator (ennreal.to_nnreal ∘ f) x :=
-begin
-  simp_rw [set.indicator_apply],
-  split_ifs,
-  { exact rfl },
-  { exact ennreal.zero_to_nnreal }
-end
-
-lemma indicator_apply_eq_top {α : Type*} (s : set α) (f : α → ℝ≥0∞) (x : α) :
-  s.indicator f x = ⊤ ↔ x ∈ s ∧ f x = ⊤ :=
-begin
-  rw [set.indicator_apply],
-  split_ifs;
-  simp only [h, true_and, zero_ne_top, false_and]
-end
+end sum
 
 end ennreal
 
-/-- Version of `tsum_ite_eq_extract` for `nnreal` rather than `topological_add_group`. -/
-lemma nnreal.tsum_ite_eq_extract [decidable_eq β]
-  {f : β → ℝ≥0} (hf : summable f) (b : β) :
+section extract
+
+/-- Version of `tsum_ite_eq_extract` for `add_comm_monoid` rather than `add_comm_group`.
+Requires a different convergence assumption involving `function.update` -/
+lemma tsum_ite_eq_extract' {α β : Type*} [topological_space α] [add_comm_monoid α] [t2_space α]
+  [has_continuous_add α] {f : β → α} (b : β) (hf : summable (f.update b 0)) :
   ∑' x, f x = f b + ∑' x, ite (x = b) 0 (f x) :=
 calc ∑' x, f x = ∑' x, ((ite (x = b) (f x) 0) + (ite (x = b) 0 (f x))) :
     tsum_congr (λ n, by split_ifs; simp only [zero_add, add_zero])
   ... = ∑' x, ite (x = b) (f x) 0 + ∑' x, ite (x = b) 0 (f x) :
-    by refine tsum_add (nnreal.summable_of_le (λ b', _) hf) (nnreal.summable_of_le (λ b', _) hf);
-      split_ifs; simp only [le_rfl, zero_le']
+    have (λ (b' : β), ite (b' = b) 0 (f b')) = function.update f b 0,
+    from funext (λ b', symm $ f.update_apply b 0 b'),
+    tsum_add ⟨ite (b = b) (f b) 0, has_sum_single b (λ b hb, if_neg hb)⟩ (this.symm ▸ hf)
   ... = (ite (b = b) (f b) 0) + ∑' x, ite (x = b) 0 (f x) :
     by { congr, exact (tsum_eq_single b (λ b' hb', if_neg hb')) }
   ... = f b + ∑' x, ite (x = b) 0 (f x) :
     by { congr, exact (if_pos rfl) }
 
-#check tsum_ite_eq_extract
-
-#check summable_of_nonneg_of_le
-
-#check summable.summable_of_eq_zero_or_self
-
-lemma summable.help {α β : Type*} [topological_space α] [add_comm_monoid α] [t2_space α]
-  [has_continuous_add α] {f g : β → α} (hf : summable f)
-  (h : ∀ b, g b = 0 ∨ g b = f b) : summable g :=
+lemma nnreal.tsum_ite_eq_extract {f : β → ℝ≥0} (hf : summable f) (b : β) :
+  ∑' x, f x = f b + ∑' x, ite (x = b) 0 (f x) :=
 begin
-  have := summable
+  refine tsum_ite_eq_extract' b (nnreal.summable_of_le (λ b', _) hf),
+  rw [function.update_apply],
+  split_ifs; simp only [zero_le', le_rfl]
 end
 
-lemma tsum_ite_eq_extract' {α β : Type*} [topological_space α] [add_comm_monoid α] [t2_space α]
-  [has_continuous_add α]
-  [decidable_eq β]
-  {f : β → α} (hf : summable f) (b : β) :
+lemma ennreal.tsum_ite_eq_extract {f : β → ℝ≥0∞} (hf : summable f) (b : β) :
   ∑' x, f x = f b + ∑' x, ite (x = b) 0 (f x) :=
-calc ∑' x, f x = ∑' x, ((ite (x = b) (f x) 0) + (ite (x = b) 0 (f x))) :
-    tsum_congr (λ n, by split_ifs; simp only [zero_add, add_zero])
-  ... = ∑' x, ite (x = b) (f x) 0 + ∑' x, ite (x = b) 0 (f x) :
-    begin
-      refine tsum_add _ _,
-      {
-        refine summable.ite
-      }
-    end
-    
-    -- by refine tsum_add (nnreal.summable_of_le (λ b', _) hf) (nnreal.summable_of_le (λ b', _) hf);
-    --   split_ifs; simp only [le_rfl, zero_le']
-  ... = (ite (b = b) (f b) 0) + ∑' x, ite (x = b) 0 (f x) :
-    by { congr, exact (tsum_eq_single b (λ b' hb', if_neg hb')) }
-  ... = f b + ∑' x, ite (x = b) 0 (f x) :
-    by { congr, exact (if_pos rfl) }
+tsum_ite_eq_extract' b ennreal.summable
 
+end extract
 
-lemma nnreal.tsum_option [decidable_eq α] {f : option α → ℝ≥0} (hf : summable f) :
+section option
+
+lemma nnreal.tsum_option {f : option α → ℝ≥0} (hf : summable f) :
   tsum f = f none + ∑' (a : α), f (some a) :=
 calc ∑' (x : option α), f x
-  = f none + ∑' (x : option α), ite (x = none) 0 (f x) : nnreal.tsum_ite_eq_extract hf none
+  = f none + ∑' (x : option α), ite (x = none) 0 (f x) : begin
+    convert @nnreal.tsum_ite_eq_extract (option α) f hf none,
+    ext x, split_ifs,
+    refl, refl,
+  end
   ... = f none + ∑' (a : α), f (some a) : begin
     refine congr_arg (λ x, f none + x) (tsum_eq_tsum_of_ne_zero_bij (λ a, some a.1) _ _ _),
     { simp only [subtype.val_eq_coe, imp_self, set_coe.forall, implies_true_iff] },
@@ -179,4 +137,4 @@ calc ∑' (x : option α), f x
     { simp only [subtype.val_eq_coe, if_false, eq_self_iff_true, implies_true_iff] }
 end
 
-end tsum
+end option
