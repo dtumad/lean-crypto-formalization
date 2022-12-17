908c491f9994587111c31dba3d9f1b4a32420655
Devon Tuma <dtumad@gmail.com>
2022-12-16 22:03:50 -0600

    coercion lemmas

--- src/computational_monads/coercions/append.lean
+++ src/computational_monads/coercions/append.lean
@@ -47,7 +47,6 @@ instance coe_append_right (α) : has_coe (oracle_comp spec α) (oracle_comp (spe
 
 variable (oa : oracle_comp spec α)
 
--- TODO: other versions of this
 lemma coe_append_right.def : (↑oa : oracle_comp (spec ++ spec') α) =
   oa.default_simulate' ⟪λ i t, let i' : (spec ++ spec').ι := sum.inl i in query i' t⟫ := rfl
 
@@ -59,8 +58,6 @@ begin
   apply_instance,
 end
 
-section support
-
 /-- Coercing to an extra oracle on the right doesn't affect a computation's `support` -/
 @[simp] lemma support_coe_append_right :
   (↑oa : oracle_comp (spec ++ spec') α).support = oa.support :=
@@ -70,12 +67,6 @@ stateless_oracle.support_simulate'_eq_support _ _ () (λ _ _, support_query _ _)
 @[simp] lemma fin_support_coe_append_right [spec.finite_range] [spec'.finite_range] [oa.decidable] :
   (↑oa : oracle_comp (spec ++ spec') α).fin_support = oa.fin_support :=
 by rw [fin_support_eq_fin_support_iff_support_eq_support, support_coe_append_right]
-
-end support
-
-section distribution_semantics
-
-open distribution_semantics
 
 /-- Coercing to an extra oracle on the right doesn't affect a computation's `eval_dist` -/
 @[simp] lemma eval_dist_coe_append_right [spec.finite_range] [spec'.finite_range] :
@@ -87,17 +78,48 @@ stateless_oracle.eval_dist_simulate'_eq_eval_dist _ _ _ (λ i t, rfl)
   ⁅e | (↑oa : oracle_comp (spec ++ spec') α)⁆ = ⁅e | oa⁆ :=
 prob_event_eq_of_eval_dist_eq (eval_dist_coe_append_right _ _ oa) e
 
-end distribution_semantics
-
 end coe_append_right
 
 section coe_append_left
 
 /-- Coerce a computation to one with access to another oracle on the left,
   forwarding the old queries to the left side of the combined set of oracles -/
-@[priority std.priority.default-50]
+@[priority std.priority.default-51]
 instance coe_append_left (α) : has_coe (oracle_comp spec α) (oracle_comp (spec' ++ spec) α) :=
 { coe := default_simulate' ⟪λ i, let i' : (spec' ++ spec).ι := sum.inr i in query i'⟫ }
+
+variable (oa : oracle_comp spec α)
+
+lemma coe_append_left.def : (↑oa : oracle_comp (spec' ++ spec) α) =
+  oa.default_simulate' ⟪λ i t, let i' : (spec' ++ spec).ι := sum.inr i in query i' t⟫ := rfl
+
+instance coe_append_left.decidable [oa.decidable] :
+  (↑oa : oracle_comp (spec' ++ spec) α).decidable :=
+begin
+  refine @simulate'.decidable _ _ _ _ _ oa () _ _ (λ i t s, _),
+  rw [stateless_oracle.apply_eq],
+  apply_instance,
+end
+
+/-- Coercing to an extra oracle on the left doesn't affect a computation's `support` -/
+@[simp] lemma support_coe_append_left :
+  (↑oa : oracle_comp (spec' ++ spec) α).support = oa.support :=
+stateless_oracle.support_simulate'_eq_support _ _ () (λ _ _, support_query _ _)
+
+/-- Coercing to an extra oracle on the left doesn't affect a computation's `fin_support`-/
+@[simp] lemma fin_support_coe_append_left [spec.finite_range] [spec'.finite_range] [oa.decidable] :
+  (↑oa : oracle_comp (spec' ++ spec) α).fin_support = oa.fin_support :=
+by rw [fin_support_eq_fin_support_iff_support_eq_support, support_coe_append_left]
+
+/-- Coercing to an extra oracle on the left doesn't affect a computation's `eval_dist` -/
+@[simp] lemma eval_dist_coe_append_left [spec.finite_range] [spec'.finite_range] :
+  ⁅(↑oa : oracle_comp (spec' ++ spec) α)⁆ = ⁅oa⁆ :=
+stateless_oracle.eval_dist_simulate'_eq_eval_dist _ _ _ (λ i t, rfl)
+
+/-- Coercing to an extra oracle on the left doesn't affect a computation's `prob_event` -/
+@[simp] lemma prob_event_coe_append_left [spec.finite_range] [spec'.finite_range] (e : set α) :
+  ⁅e | (↑oa : oracle_comp (spec' ++ spec) α)⁆ = ⁅e | oa⁆ :=
+prob_event_eq_of_eval_dist_eq (eval_dist_coe_append_left _ _ oa) e
 
 end coe_append_left
 
@@ -110,6 +132,44 @@ instance coe_append_right_of_coe (α) [has_coe (oracle_comp coe_spec α) (oracle
   has_coe (oracle_comp coe_spec α) (oracle_comp (coe_spec' ++ spec) α) :=
 { coe := λ oa, ↑oa }
 
+variables [has_coe (oracle_comp coe_spec α) (oracle_comp coe_spec' α)]
+  (oa : oracle_comp coe_spec α)
+
+lemma coe_append_right_of_coe.def : (↑oa : oracle_comp (coe_spec' ++ spec) α) =
+  ↑(oa : oracle_comp coe_spec' α) := rfl
+
+instance coe_append_right_of_coe.decidable [(↑oa : oracle_comp coe_spec' α).decidable] :
+  (↑oa : oracle_comp (coe_spec' ++ spec) α).decidable :=
+begin
+  rw [coe_append_right_of_coe.def],
+  refine @simulate'.decidable _ _ _ _ _ _ () _ _ (λ i t s, _),
+  rw [stateless_oracle.apply_eq],
+  apply_instance,
+end
+
+/-- Coercing to an extra oracle on the left doesn't affect a computation's `support` -/
+@[simp] lemma support_coe_append_right_of_coe :
+  (↑oa : oracle_comp (coe_spec' ++ spec) α).support = (↑oa : oracle_comp coe_spec' α).support :=
+by rw [coe_append_right_of_coe.def, support_coe_append_right]
+
+/-- Coercing to an extra oracle on the left doesn't affect a computation's `fin_support`-/
+@[simp] lemma fin_support_coe_append_right_of_coe [spec.finite_range] [coe_spec'.finite_range]
+  [oa.decidable] [(↑oa : oracle_comp coe_spec' α).decidable] :
+  (↑oa : oracle_comp (coe_spec' ++ spec) α).fin_support =
+    (↑oa : oracle_comp coe_spec' α).fin_support :=
+by rw [fin_support_eq_fin_support_iff_support_eq_support, support_coe_append_right_of_coe]
+
+/-- Coercing to an extra oracle on the left doesn't affect a computation's `eval_dist` -/
+@[simp] lemma eval_dist_coe_append_right_of_coe [spec.finite_range] [coe_spec'.finite_range] :
+  ⁅(↑oa : oracle_comp (coe_spec' ++ spec) α)⁆ = ⁅(↑oa : oracle_comp coe_spec' α)⁆ :=
+stateless_oracle.eval_dist_simulate'_eq_eval_dist _ _ _ (λ i t, rfl)
+
+/-- Coercing to an extra oracle on the left doesn't affect a computation's `prob_event` -/
+@[simp] lemma prob_event_coe_append_right_of_coe [spec.finite_range] [coe_spec'.finite_range]
+  (e : set α) : ⁅e | (↑oa : oracle_comp (coe_spec' ++ spec) α)⁆ =
+    ⁅e | (↑oa : oracle_comp coe_spec' α)⁆ :=
+prob_event_eq_of_eval_dist_eq (eval_dist_coe_append_right_of_coe _ _ _ _) e
+
 end coe_append_right_of_coe
 
 section coe_right_side_append
@@ -119,17 +179,76 @@ section coe_right_side_append
 instance coe_right_side_append (α)
   [∀ α, has_coe (oracle_comp coe_spec α) (oracle_comp coe_spec' α)] :
   has_coe (oracle_comp (spec ++ coe_spec) α) (oracle_comp (spec ++ coe_spec') α) :=
-{ coe := λ oc, oc.default_simulate' ⟪λ i, match i with
+{ coe := λ oa, oa.default_simulate' ⟪λ i, match i with
   | (sum.inl i') := (λ t, (query i' t : oracle_comp (spec ++ coe_spec') (spec.range i')))
   | (sum.inr i') := (λ t, (query i' t : oracle_comp (spec ++ coe_spec') (coe_spec.range i')))
-end ⟫ }
+end⟫ }
+
+variables [∀ α, has_coe (oracle_comp coe_spec α) (oracle_comp coe_spec' α)]
+  (oa : oracle_comp (spec ++ coe_spec) α)
+
+lemma coe_right_side_append.def : (↑oa : oracle_comp (spec ++ coe_spec') α) =
+  oa.default_simulate' ⟪λ i, sum.rec_on i
+    (λ i' t, (↑(query i' t) : oracle_comp (spec ++ coe_spec') (spec.range i')))
+    (λ i' t, (query i' t : oracle_comp (spec ++ coe_spec') (coe_spec.range i')))⟫ :=
+begin
+  refine congr_arg (λ o, oa.default_simulate' ⟪o⟫) (funext $ λ i, _),
+  cases i; refl
+end
+
+instance coe_right_side_append.decidable [oa.decidable]
+  [h : ∀ α (oa' : oracle_comp coe_spec α) [oa'.decidable],
+    (↑oa' : (oracle_comp coe_spec' α)).decidable] :
+  (↑oa : oracle_comp (spec ++ coe_spec') α).decidable :=
+begin
+  rw [coe_right_side_append.def],
+  refine @simulate'.decidable _ _ _ _ _ oa () _ _ (λ i t s, _),
+  rw [stateless_oracle.apply_eq],
+  cases i,
+  { apply_instance },
+  { exact @oracle_comp.decidable_bind _ _ _ _ _
+      (@coe_append_left.decidable _ _ _ _ (h _ (query i t))) _ }
+end
+
+/-- Coercing to an extra oracle on the right doesn't affect a computation's `support` -/
+@[simp] lemma support_coe_right_side_append :
+  (↑oa : oracle_comp (spec ++ coe_spec') α).support = oa.support :=
+begin
+  rw [coe_right_side_append.def],
+  refine stateless_oracle.support_simulate'_eq_support _ _ () (λ i t, _),
+  cases i,
+  {
+    simp only [support_coe_append_right, support_query],
+  },
+  {
+    simp only [coe_coe, support_coe_append_left, set.top_eq_univ],
+    sorry
+  }
+end
+-- stateless_oracle.support_simulate'_eq_support _ _ () (λ _ _, support_query _ _)
+
+-- /-- Coercing to an extra oracle on the right doesn't affect a computation's `fin_support`-/
+-- @[simp] lemma fin_support_coe_right_side_append [spec.finite_range] [spec'.finite_range] [oa.decidable] :
+--   (↑oa : oracle_comp (spec ++ spec') α).fin_support = oa.fin_support :=
+-- by rw [fin_support_eq_fin_support_iff_support_eq_support, support_coe_append_right]
+
+-- /-- Coercing to an extra oracle on the right doesn't affect a computation's `eval_dist` -/
+-- @[simp] lemma eval_dist_coe_right_side_append [spec.finite_range] [spec'.finite_range] :
+--   ⁅(↑oa : oracle_comp (spec ++ spec') α)⁆ = ⁅oa⁆ :=
+-- stateless_oracle.eval_dist_simulate'_eq_eval_dist _ _ _ (λ i t, rfl)
+
+-- /-- Coercing to an extra oracle on the right doesn't affect a computation's `prob_event` -/
+-- @[simp] lemma prob_event_coe_right_side_append [spec.finite_range] [spec'.finite_range] (e : set α) :
+--   ⁅e | (↑oa : oracle_comp (spec ++ spec') α)⁆ = ⁅e | oa⁆ :=
+-- prob_event_eq_of_eval_dist_eq (eval_dist_coe_append_right _ _ oa) e
+
 
 end coe_right_side_append
 
 section coe_left_side_append
 
 /-- Coerce the oracle on the left side of an existing set of appended oracles -/
-@[priority std.priority.default+5]
+@[priority std.priority.default+4]
 instance coe_left_side_append (α)
   [∀ α, has_coe (oracle_comp coe_spec α) (oracle_comp coe_spec' α)] :
   has_coe (oracle_comp (coe_spec ++ spec) α) (oracle_comp (coe_spec' ++ spec) α) :=
@@ -226,8 +345,8 @@ example (oa : oracle_comp ((spec ++ spec') ++ (spec'' ++ coe_spec')) α) :
   oracle_comp (spec ++ spec' ++ spec'' ++ coe_spec') α := ↑oa
 
 /-- coercion makes it possible to mix computations on individual oracles -/
-noncomputable example {spec : oracle_spec} : oracle_comp (uniform_selecting ++ spec) bool := 
-do { n ←$[0..10], if n ≤ 3 then return ff else coin }
+example {spec : oracle_spec} : oracle_comp (uniform_selecting ++ spec) bool := 
+do { n ←$[0..10], b ← coin, if n ≤ 3 ∧ b = tt then return ff else coin }
 
 end examples
 
