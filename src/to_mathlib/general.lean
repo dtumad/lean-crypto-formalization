01b2a5e7b9bd44a1d34968083125f024d7b69482
Devon Tuma <dtumad@gmail.com>
2023-03-24 17:02:54 -0500

    repeat stuff

--- src/to_mathlib/general.lean
+++ src/to_mathlib/general.lean
@@ -22,34 +22,56 @@ lemma vector.cons_eq_cons {n : ℕ} (x y : α) (xs ys : vector α n) :
 ⟨λ h, have x = y ∧ xs.to_list = ys.to_list, by simpa only [vector.to_list_cons]
   using congr_arg vector.to_list h, ⟨this.1, vector.eq _ _ this.2⟩, λ h, h.1 ▸ h.2 ▸ rfl⟩
 
-lemma list.not_mem_repeat_zero (x y : α) :
-  y ∉ (list.repeat x 0) := by simp only [list.repeat, list.not_mem_nil, not_false_iff]
+section list_stuff
 
-lemma list.mem_repeat_succ (n : ℕ) (x y : α) :
-  y ∈ (list.repeat x n.succ) ↔ y = x :=
+variables (x : α) (n : ℕ)
+
+@[simp] lemma list.mem_repeat_iff (y : α) : y ∈ (list.repeat x n) ↔ n ≠ 0 ∧ y = x :=
+begin
+  induction n with n hn,
+  { rw [list.repeat, list.mem_nil_iff, ne.def, ne_self_iff_false, false_and] },
+  { simp only [hn, list.repeat, list.mem_cons_iff, ne.def, nat.succ_ne_zero, not_false_iff,
+      true_and, or_iff_left_iff_imp, and_imp, imp_self, implies_true_iff] }
+end
+
+lemma list.not_mem_repeat_zero (y : α) : y ∉ (list.repeat x 0) :=
+by simp only [list.mem_repeat_iff, ne.def, ne_self_iff_false, false_and, not_false_iff]
+
+lemma list.mem_repeat_succ_iff (y : α) : y ∈ (list.repeat x n.succ) ↔ y = x :=
+by simp only [list.mem_repeat_iff, ne.def, nat.succ_ne_zero, not_false_iff, true_and]
+
+@[simp] lemma list.nth_repeat_eq_none_iff (m : ℕ) : (list.repeat x n).nth m = none ↔ n < m :=
+sorry
+
+@[simp] lemma list.nth_repeat_eq_some_iff (m : ℕ) (y : α) :
+  (list.repeat x n).nth m = some y ↔ m ≤ n ∧ y = x :=
+sorry
+
+lemma list.nth_repeat (m : ℕ) : (list.repeat x n).nth m = if m ≤ n then some x else none :=
+sorry
+
+@[simp] lemma list.find_repeat_eq_none_iff  (p : α → Prop) [decidable_pred p] :
+  (list.repeat x n).find p = none ↔ n = 0 ∨ ¬ p x :=
+sorry
+
+@[simp] lemma list.find_repeat_eq_some_iff (p : α → Prop) [decidable_pred p] (y : α) :
+  (list.repeat x n).find p = some y ↔ 0 < n ∧ p x ∧ y = x :=
+sorry
+
+lemma list.find_repeat (p : α → Prop) [decidable_pred p] :
+  (list.repeat x n).find p = if p x then some x else none :=
 begin
   sorry
 end
 
-lemma list.eq_of_mem_repeat {n : ℕ} {x y : α} (hy : y ∈ list.repeat x n) : y = x :=
-begin
-  cases n with n,
-  { exact false.elim (list.not_mem_repeat_zero x y hy) },
-  { exact (list.mem_repeat_succ n x y).1 hy }
-end
-
-lemma list.find_repeat (x : α) (n : ℕ) (p : α → Prop) [decidable_pred p] :
-  (list.repeat x n).find p = if p x then some x else none :=
-begin
-  sorry,
-end
+end list_stuff
 
 lemma vector.mem_repeat_succ (n : ℕ) (x y : α) :
   y ∈ (vector.repeat x n.succ).to_list ↔ y = x :=
 begin
   rw [vector.repeat, vector.to_list],
   simp [list.repeat],
-  sorry
+  -- sorry
 end
 
 -- lemma vector.zip_with_comp {n : ℕ} (f : α → β) (g : β → γ) (v : vector α n) :
