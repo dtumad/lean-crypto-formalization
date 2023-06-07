49d7d0e4c60cf0b1c362827429d9a76a5db5dc2e
Devon Tuma <dtumad@gmail.com>
2023-06-06 23:34:05 -0500

    presentation prep

--- src/computational_monads/distribution_semantics/tactics.lean
+++ src/computational_monads/distribution_semantics/tactics.lean
@@ -8,8 +8,7 @@ import computational_monads.distribution_semantics.monad
 /-!
 # Induction Tactics for Distribution Semantics of `oracle_comp`
 
-This file defines custom tactics for working with `support`, `eval_dist`, and `prob_event`,
-specifically focusing on using `dist_equiv` for rewriting expressions.
+This file defines custom tactics for working with and proving instances of `dist_equiv`.
 -/
 
 open interactive interactive.types tactic
@@ -18,22 +17,23 @@ variables {α β γ : Type} {spec : oracle_spec}
 
 namespace tactic.interactive.oracle_comp
 
-meta def rw_dist_equiv_base (d_eq : expr) : tactic unit :=
-tactic.apply d_eq >> return () <|>
-  tactic.refine ``(symm _) >> tactic.apply d_eq >> return () <|>
-    return ()
+private meta def rw_dist_equiv_base (d_eq : expr) : tactic bool :=
+tactic.apply d_eq >> return tt <|>
+  tactic.refine ``(symm _) >> tactic.apply d_eq >> return tt <|>
+    return ff
 
 /-- Attempt to discharge a distributional equivalence between two computations by todo. -/
-meta def pairwise_dist_equiv_base (d_eqs : list pexpr) : tactic unit :=
+private meta def pairwise_dist_equiv_base (d_eqs : list pexpr) : tactic unit :=
 do `(%%oa ≃ₚ %%oa') ← tactic.target <|> tactic.fail ("Goal must be a distributional equivalence"),
   -- Immedeately discharge the goal if the equivalence is between equal objects
   (tactic.unify oa oa' >> tactic.congr) <|>
-    (d_eqs.mmap (λ d, tactic.to_expr d >>= rw_dist_equiv_base) >> return ())
+    (d_eqs.mmap (λ d, tactic.to_expr d >>= rw_dist_equiv_base) >>=
+      λ bs, tactic.rotate (bs.count tt))
 
 /-- Destruct a distributional equivalence by recursively splitting equivalences between binds
 into multiple equivalences between their individual components.
 For other equivalences will defer to `pairwise_dist_equiv_base`. -/
-meta def destruct_pairwise_dist_equiv (d_eq : list pexpr) : expr → tactic unit
+private meta def destruct_pairwise_dist_equiv (d_eq : list pexpr) : expr → tactic unit
 | `((%%oa >>= %%ob ≃ₚ %%oa' >>= %%ob')) := do {
   -- Check that the first components of each bind operation have the same type.
   `(oracle_comp %%spec %%α) ← tactic.infer_type oa,
@@ -78,22 +78,25 @@ example (oa oa' : oracle_comp spec α) (ob ob' : α → oracle_comp spec β)
   (oc oc' : α → β → oracle_comp spec γ) (h : oa ≃ₚ oa') (h' : ∀ a, ob a ≃ₚ ob' a)
   (h'' : ∀ a b, oc a b ≃ₚ oc' a b) :
   (oa >>= λ x, ob x >>= λ y, oc x y) ≃ₚ (oa' >>= λ x, ob' x >>= λ y, oc' x y) :=
-by {oracle_comp.pairwise_dist_equiv, apply h'', apply h, apply h'}
+begin
+  oracle_comp.pairwise_dist_equiv,
+  apply h'', apply h, apply h'
+end
 
 /-- If two bind operations have mismatched intermediate types, but there is an existing equivalence
 between the two, then `pairwise_dist_equiv` should be able to solve without error. -/
 example (oa : oracle_comp spec α) (oa' : oracle_comp spec γ)
   (ob : α → oracle_comp spec β) (ob' : γ → oracle_comp spec β)
-  (h : oa >>= ob ≃ₚ oa' >>= ob') :
-  oa >>= ob ≃ₚ oa' >>= ob' :=
+  (h : oa >>= ob ≃ₚ oa' >>= ob') : oa >>= ob ≃ₚ oa' >>= ob' :=
 by oracle_comp.pairwise_dist_equiv [h]
 
-/-- `pairwise_dist_equiv` should be able to handle nested branching of binding operations. -/
-example (oa : oracle_comp spec α) (ob ob' : α → oracle_comp spec β)
-  (oc oc' : α → β → oracle_comp spec α)
-  (h : ∀ a, ob a ≃ₚ ob' a) (h' : ∀ a b, oc a b ≃ₚ oc' a b) :
-  ((oa >>= λ x, ob x >>= λ y, oc x y) >>= λ x, ob x >>= λ y, oc x y) ≃ₚ
-    ((oa >>= λ x, ob x >>= λ y, oc' x y) >>= λ x, ob' x >>= λ y, oc' x y) :=
-by oracle_comp.pairwise_dist_equiv [h, h']
+/-- `pairwise_dist_equiv` should be able to handle nested branching of binding operations,
+as well as equivalence arguments with many parameters -/
+example (oa oa' : oracle_comp spec α) (hoa : oa ≃ₚ oa')
+  (ob ob' : α → oracle_comp spec β) (hob : ∀ a, ob a ≃ₚ ob' a)
+  (oc oc' : α → β → oracle_comp spec α) (hoc : ∀ a b, oc a b ≃ₚ oc' a b) :
+  ((oa' >>= λ x, ob x >>= λ y, oc x y) >>= λ x, ob x >>= λ y, oc' x y) ≃ₚ
+    ((oa >>= λ x, ob' x >>= λ y, oc' x y) >>= λ x, ob' x >>= λ y, oc' x y) :=
+by oracle_comp.pairwise_dist_equiv [hoa, hob, hoc]
 
 end examples
